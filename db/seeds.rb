bb76da8dc7ca2a0f5ae792679eb3d00f42e2d722
der Freidenker <barranzuelald@gmail.com>
2024-06-13 15:49:41 -0500

    resuelto

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'open-uri'
+require 'json'
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+response = URI.open(url).read
+movies = JSON.parse(response)['results']
+
+movies.each do |movie|
+  Movie.create!(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}",
+    rating: movie['vote_average']
+  )
+end
